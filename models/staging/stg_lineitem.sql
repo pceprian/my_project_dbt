3ef2612ce96d68f529efb51459813a95623de236
Paula Bayon <paula.ceprian@sdggroup.com>
2026-05-27 08:54:46 +0000

    Errors regarding stg_lineitem solved.

--- models/staging/stg_lineitem.sql
+++ models/staging/stg_lineitem.sql
@@ -1,15 +1,25 @@
 {{ config(
+
     materialized='incremental',
+
     unique_key='lineitem_id'
+
    )
+
 }}
 
+
+
 with
+
 source as (
+
   select * from {{ source('snowflake_sample', 'lineitem') }}
+
 ),
 
 transformed as (
+
   select
     concat(l_orderkey, '-', l_partkey, '-', l_suppkey,'-', l_linenumber) as lineitem_id,
     l_orderkey as order_id,
@@ -19,21 +29,17 @@ transformed as (
     l_quantity as lineitem_quantity,
     l_extendedprice as lineitem_extended_price,
     {{ safe_divide_zero('l_extendedprice', '1 - l_discount') }} as sum_disc_price,
-    {{ safe_divide_zero(safe_divide_zero('l_extendedprice', '1 - l_discount'), '1 + l_tax') }} AS sum_charge,
+    {{ safe_divide_zero(safe_divide_zero('l_extendedprice', '1 - l_discount'), '1 + l_tax') }} as sum_charge,
     l_discount as lineitem_discount,
     l_tax as lineitem_tax,
-    l_returnflag as lineitem_return_flag,
     l_linestatus as line_status,
+    l_returnflag as lineitem_return_flag,
     {{ replace_null('cast(l_shipdate as date)', 'No Date') }} as lineitem_ship_date,
     {{ replace_null('cast(l_commitdate as date)', 'No Date') }} as lineitem_commit_date,
     {{ replace_null('cast(l_receiptdate as date)', 'No Date') }} as lineitem_receipt_date,
     l_shipinstruct as lineitem_ship_instruct,
     l_shipmode as lineitem_ship_mode,
-    {{ insert_timestamp() }} AS loaded_at
+    {{ insert_timestamp() }} as loaded_at
   from source
 )
 select * from transformed
-
-{% if is_incremental() %}
-  where lineitem_ship_date > (select max(lineitem_ship_date) from {{ this }})
-{% endif %}
