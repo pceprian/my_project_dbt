e8abe64b31783e6912bf8d8f92c1d9d76e6cde4e
Paula Bayon <paula.ceprian@sdggroup.com>
2026-06-09 11:08:35 +0000

    Deleted macro size_parts because it was a very concrete case when, and macros need to be built and used for more global purposes. The macro was only used at stg_parts.sql, so I directly applied the case when there instead

--- models/staging/stg_parts.sql
+++ models/staging/stg_parts.sql
@@ -10,7 +10,12 @@ transformed as (
     p_brand as part_brand,
     p_type as part_type,
     p_size as part_size,
-    {{ size_parts('p_size') }} as part_size_group,
+    case 
+        when p_size < 20 then 'Small'
+        when p_size >= 20 and p_size < 40 then 'Medium'
+        when p_size >= 40 then 'Large'
+        else 'Unknown'
+    end as part_size_group,
     p_container as part_container,
     p_retailprice as part_retail_price,
     {{ dbt_utils.current_timestamp() }} as loaded_at
