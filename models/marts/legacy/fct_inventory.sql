fae457a107e6bcc2ca7c9a5f01d890eec6a68210
Paula Bayon <paula.ceprian@sdggroup.com>
2026-06-11 10:59:15 +0000

    changes in fct_inventory

--- models/marts/legacy/fct_inventory.sql
+++ models/marts/legacy/fct_inventory.sql
@@ -1,6 +1,11 @@
 with
 partsupp as (
-    select * from {{ ref('stg_partsupp') }}
+    select 
+        part_id,
+        supplier_id,
+        part_supplier_available_quantity,
+        part_supply_cost
+    from {{ ref('stg_partsupp') }}
 ),
 dim_parts as (
     select * from {{ ref('dim_parts') }}
@@ -16,7 +21,7 @@ inventory_calculations as (
         supplier_id,
         part_supplier_available_quantity,
         part_supply_cost,
-        (part_supplier_available_quantity * part_supply_cost) as total_inventory_valuation
+        (coalesce(part_supplier_available_quantity, 0) * coalesce(part_supply_cost, 0)) as total_inventory_valuation
     from partsupp
 )
 
